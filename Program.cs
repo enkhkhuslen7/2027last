6905186b3e7f44cbd35a116aac35dd09966cb5c1
ENMDC <tcapg22503030@gmail.com>
2026-02-12 14:46:16 +0900

    問題4(本物)

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MyApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter Your Number!");
+            string input = Console.ReadLine();
+            float number = float.Parse(input);
+            if(number / 2 == 0)
+            {
+                Console.WriteLine("The number is Integer.");
+            }
+            else
+            {
+                Console.WriteLine("The number is not Integer.");
+            }
+        }
+    }
+}
